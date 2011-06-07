b9b7c4617f0ac7c13456441f609f4eb4ba931209
Davidh <dhutchison86+github@googlemail.com>
2011-06-07 22:16:23 +0100

    Changed so not dependent on the script being run from the same directory

--- TwitterFavouritesToTHL/importTwitterFavsToTHL.sh
+++ TwitterFavouritesToTHL/importTwitterFavsToTHL.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+#Keep a handle on the current working dir
+currentDir=$(pwd)
+#Change to the directory this script is located in
+scriptDir=`dirname "$0"`
+cd "$scriptDir"
+# Run the ruby script
 ruby -rubygems TwitterFavouritesToTHL.rb
+#Change back to the original working dir
+cd $currentDir
