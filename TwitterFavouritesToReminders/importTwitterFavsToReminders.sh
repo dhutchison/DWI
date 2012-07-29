24ec318fe4fa5ea6591fb8bfd04544c761bcb3a1
Davidh <dhutchison86+github@googlemail.com>
2012-07-29 21:44:57 +0100

    Initial Commit
    New version of the TwitterFavouritesToTHL script to update the new
    Reminders app under 10.8 Mountain Lion.

---
+++ TwitterFavouritesToReminders/importTwitterFavsToReminders.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Keep a handle on the current working dir
+currentDir=$(pwd)
+#Change to the directory this script is located in
+scriptDir=`dirname "$0"`
+cd "$scriptDir"
+# Run the ruby script
+ruby -rubygems TwitterFavouritesToReminders.rb
+#Change back to the original working dir
+cd "$currentDir"
