edb3b757c1d10aff55e44e378b90e0b2d85e2ea9
Davidh <dhutchison86+github@googlemail.com>
2012-10-18 22:41:19 +0100

    Merge of projects importing twitter favourites. Now requires API keys.

---
+++ TwitterFavouritesToList/TwitterFavouritesToList.rb
@@ -0,0 +1,176 @@
+=begin
+
+Purpose:
+
+Script to add all twitter favourites for a user into a TODO list application. 
+The application used in based on the configuration file. 
+
+This script is configured through a .twitter file, which should exist in the users 
+home directory. A sample.twitter file is provided to show the basic file format. 
+
+The "import_application" element has two possible values, which changes the application that 
+the import will be performed to:
+  * "THL" will use The Hit List, with items inserted into the Inbox.
+  * "Reminders" will use the Reminders app under OS X 10.8 Mountain Lion, with items inserted into the Inbox.
+
+This file will be updated by this script to keep a handle on the newest tweet which 
+a task was created for. The element "lastProcessedTweet" will be added to keep track of
+the id of the last tweet processed, to prevent processing the same tweet multiple times.
+
+The created task will have a title in the form:
+"Tweet from <user display name>"
+and a note in the form:
+"<tweet text>
+
+<link to tweet>"
+The start date for the task is date the task was run. Originally (and still commented out), the creation date of the tweet was used instead.
+
+Requires: 
+sudo gem install twitter
+sudo gem install rb-appscript
+
+Author: David Hutchison
+http://devwithimagination.wordpress.com/
+
+=end
+
+require_relative 'ConfigStore'
+require 'twitter'
+require 'appscript'
+
+include Appscript
+
+#Using a float here so we can use ceil in division operations
+MAXIMUM_FAVOURITES_PER_PAGE = 20.0
+
+
+#Load in information from the configuration
+twitterConfig = ConfigStore.new( "#{ENV['HOME']}/.twitter" )
+username = twitterConfig['username']
+raise ArgumentError, 'username configuration option missing' if username == nil
+context = twitterConfig['new_task_context']
+lastProcessedTweet = twitterConfig['lastProcessedTweet']
+
+#Configure authentication
+raise ArgumentError, 'consumer_key configuration option missing' if twitterConfig['consumer_key'] == nil
+raise ArgumentError, 'consumer_secret configuration option missing' if twitterConfig['consumer_secret'] == nil
+raise ArgumentError, 'oauth_token configuration option missing' if twitterConfig['oauth_token'] == nil
+raise ArgumentError, 'oauth_token_secret configuration option missing' if twitterConfig['oauth_token_secret'] == nil
+Twitter.configure do |config|
+  config.consumer_key = twitterConfig['consumer_key']
+  config.consumer_secret = twitterConfig['consumer_secret']
+  config.oauth_token = twitterConfig['oauth_token']
+  config.oauth_token_secret = twitterConfig['oauth_token_secret']
+end
+
+#clean up the loaded context information
+if(context != nil && !context.strip.empty?)
+	#Strip any leading or tailing whitespace
+	context = context.strip
+	if(!(context.start_with?('@')))
+		context = "@#{context}"
+	end
+else
+	#Just use a blank
+	context = ""
+end
+
+#Work out the correct application name to use
+if("THL".eql? twitterConfig['import_application'])
+	applicationName = "The Hit List"
+elsif("Reminders".eql? twitterConfig['import_application'])
+		applicationName = "Reminders"
+else
+	raise ArgumentError, 'Illegal value for import_application configuration option. Allowed values are "THL" and "Reminders".'
+end
+
+#This is far from perfect, as we cannot get an accurate count of the number 
+#of favourites since a given id. Worse case scenario is the user has a large number
+#of favourites, resulting in wasted page call queries. 
+#TODO: Fix this 
+totalFavourites = Twitter.user( username )['favourites_count']
+totalFavouritePages = (totalFavourites / MAXIMUM_FAVOURITES_PER_PAGE).ceil
+
+# if there is a possibility of loading more tweets, launch the application if required. 
+wasRunning = true
+
+application=Appscript.app(applicationName)
+begin
+  wasRunning = false
+  application.run
+end unless application.is_running?
+
+#Keep track of the latest tweet processed, will update the configuration file later
+lastTweet = nil
+
+importedTweets = 0
+
+totalFavouritePages.downto( 1 ) do |pageNumber|
+  puts "Attempting to get tweets for page number: #{pageNumber}"
+
+  begin
+    begin
+    	if(lastProcessedTweet != nil)
+      		page = Twitter.favorites( :user => username, :page => pageNumber, :since_id => lastProcessedTweet )
+      else
+      	page = Twitter.favorites( :user => username, :page => pageNumber )
+      end
+    rescue Twitter::NotFound => e
+      puts e.message
+      sleep 5
+      retry
+    rescue => e
+      puts e.message
+    end
+
+    unless (page == nil || page.empty?)
+      page.reverse.each { |tweet|
+        begin
+        if("The Hit List".eql? applicationName)
+        	application.inbox.make(:new => :task, :with_properties => {
+        	    :timing_task => "Tweet from #{tweet.user.name} #{context}",
+        	    :notes => "#{tweet.text} \n\n http://twitter.com/#{tweet.user.screen_name}/statuses/#{tweet.id}",
+        	    :start_date => Date.today
+        	    #:start_date => DateTime.parse("#{tweet.created_at}")
+        	})
+        else
+	        #Applescript dictionary defines this list as "default list", rbappscript uses an underscore to replace spaces in situations like this.
+	        application.default_list.make(:new => :reminder, :with_properties => {
+	        	:name => "Tweet from #{tweet.user.name} http://twitter.com/#{tweet.user.screen_name}/statuses/#{tweet.id} #{context}",
+	        	:body => "#{tweet.text}",
+	        })
+        end
+        
+        importedTweets += 1
+        lastTweet = tweet.id
+        
+        rescue => e
+          puts "Had a problem creating task for tweet #{tweet.id} : #{e.message}"
+          $! = TRUE
+        end
+      }
+    end
+  rescue Twitter::BadRequest => e
+    twitter_API_rate_reset_time_in_seconds = Twitter.rate_limit_status.reset_time_in_seconds
+    minutes_until_rate_limit_is_reset = ( twitter_API_rate_reset_time_in_seconds - Time.now.to_i ) / 60.0
+    puts "Sorry, we've exceeded the Twitter-imposed rate limit for accessing their service.  We'll have to wait #{minutes_until_rate_limit_is_reset} minutes before this account can access Twitter again."
+    sleep twitter_API_rate_reset_time_in_seconds
+    retry
+  rescue Twitter::ServiceUnavailable => e
+    sleep 5
+    retry
+  end
+end
+
+puts "Successfully imported #{importedTweets} tweets into #{applicationName}"
+
+if(lastTweet != nil)
+
+	twitterConfig['lastProcessedTweet'] = lastTweet
+	twitterConfig.save()
+
+end
+
+#If the application was not running when this script started, then quit it again
+application.quit if !wasRunning
+	
